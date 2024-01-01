35ca0199da8bd818aaca213df5d0254e6890ab66
Yosef Kovan <154745088+YosefKovan@users.noreply.github.com>
2024-01-01 20:54:11 +0200

    Update Triangle.h

--- include/Triangle.h
+++ include/Triangle.h
@@ -1,1 +1,30 @@
 
+#include "Vertex.h"
+#include "Rectangle.h"
+#include "Board.h"
+
+class Triangle {
+public:
+	Triangle(const Vertex vertices[3]);
+	Triangle(const Vertex& left, const Vertex& right, double height);
+	Vertex getVertex(int index) const;
+	double getLength() const;
+	double getHeight() const;
+
+	void draw(Board& board) const;
+	Rectangle getBoundingRectangle() const;
+	double getArea() const;
+	double getPerimeter() const;
+	Vertex getCenter() const;
+	bool scale(double factor);
+	
+
+private:
+	Vertex m_vertex1;
+	Vertex m_vertex2;
+	Vertex m_vertex3;
+	double calculateLength(Vertex, Vertex);
+	bool checkCond();
+	void changeTriangleVertices();
+
+};
