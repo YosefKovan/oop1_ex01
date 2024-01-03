f697ed7a233f623ea785220e698c9e867780aeaa
Chaim-Friedman <154747664+Chaim-Friedman@users.noreply.github.com>
2024-01-04 00:00:58 +0200

    Update Hexagram.h

--- include/Hexagram.h
+++ include/Hexagram.h
@@ -1,1 +1,28 @@
+#include "Vertex.h"
+#include "Rectangle.h"
+#include "Board.h"
+#include "Triangle.h"
 
+class Hexagram {
+
+public:
+	Hexagram(Vertex firstTriangle[3], Vertex secondTriangle[3]);
+	Hexagram(const Triangle& t1, const Triangle& t2);
+	double getTotalHeight() const;
+	double getLength() const;
+
+	void draw(Board& board) const;
+	Rectangle getBoundingRectangle() const;
+	double getArea() const;
+	double getPerimeter() const;
+	Vertex getCenter() const;
+	bool scale(double factor);
+
+private:
+	Triangle m_up;
+	Triangle m_down;
+	bool checkCond();
+	void DefaultHexagram();
+};
+
+	
