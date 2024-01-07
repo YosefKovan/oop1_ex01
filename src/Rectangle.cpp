1ce21c5632976617146dbd155bfa131c3730b787
Yosef Kovan <154745088+YosefKovan@users.noreply.github.com>
2024-01-07 21:53:38 +0200

    Update Rectangle.cpp

--- src/Rectangle.cpp
+++ src/Rectangle.cpp
@@ -5,80 +5,66 @@
 
 
 Rectangle::Rectangle(const Vertex& bottomLeft, const Vertex& topRight) 
-   : m_bottomLeft(bottomLeft.m_col, bottomLeft.m_row),
-	 m_topRight(topRight.m_col, topRight.m_row)
-{
-	if (checkCond(m_bottomLeft, m_topRight))
-		changeToDefault();
-	
-	
-}
+    : Rectangle(bottomLeft.m_col, bottomLeft.m_row, topRight.m_col, topRight.m_row)
+{}
 //------------------------------------------------------
 Rectangle::Rectangle(const Vertex vertices[2])
-	: m_bottomLeft(vertices[0].m_col, vertices[0].m_row),
-	  m_topRight(vertices[1].m_col, vertices[1].m_row)
-{
-	if (checkCond(m_bottomLeft, m_topRight))
-		changeToDefault();
-	 
-
-}
+	: Rectangle(vertices[0].m_col, vertices[0].m_row, 
+		        vertices[1].m_col, vertices[1].m_row)
+{}
 //------------------------------------------------------
 Rectangle::Rectangle(double x0, double y0, double x1, double y1)
 	: m_bottomLeft(x0, y0), m_topRight(x1, y1)
 {
 	if (checkCond(m_bottomLeft, m_topRight))
-		changeToDefault();
-	 
-	 
+		changeToDefault();	 
 }
 //------------------------------------------------------
 Rectangle::Rectangle(const Vertex& start, double width, double height)
-	: m_bottomLeft(start.m_col, start.m_row),
-	  m_topRight(start.m_col + std::abs(width), start.m_row + std::abs(height))
+	: m_bottomLeft(start),
+	  m_topRight(start.m_col + abs(width), start.m_row + abs(height))
 {
-	if (checkCond(m_bottomLeft, m_topRight))
-		changeToDefault();
-	  	
+	 if (checkCond(m_bottomLeft, m_topRight) || width < 0 || height < 0)
+		 changeToDefault();
 }
 //------------------------------------------------------
 bool Rectangle::checkCond(Vertex bottomLeft, Vertex topRight) {
 
-	if (bottomLeft.isHigherThan(topRight) || bottomLeft.isToTheRightOf(topRight))
-		return true;
+	 if (bottomLeft.isHigherThan(topRight) || bottomLeft.isToTheRightOf(topRight))
+		 return true;
 
-	if (!bottomLeft.isValid() || !topRight.isValid())
-		return true;
+	 if (!bottomLeft.isValid() || !topRight.isValid())
+		 return true;
 	
-	return false;
+	 return false;
 }
 //------------------------------------------------------
 void Rectangle::changeToDefault() {
 
-	m_bottomLeft.m_col = 20; 
-	m_bottomLeft.m_row = 10;  
-	m_topRight.m_col = 30;
-	m_topRight.m_row = 20;
+     m_bottomLeft.m_col = 20; 
+	 m_bottomLeft.m_row = 10;  
+	 m_topRight.m_col = 30;
+	 m_topRight.m_row = 20;
 }
 //------------------------------------------------------
 Vertex Rectangle::getBottomLeft() const{
 	
-	return  m_bottomLeft;
+	 return m_bottomLeft;
 }
 //------------------------------------------------------
 Vertex Rectangle::getTopRight() const{
 
-	return  m_topRight;
+	 return m_topRight;
 }
 //------------------------------------------------------
 double Rectangle::getWidth() const{
 	
-	return m_topRight.m_col - m_bottomLeft.m_col;
+	 return m_topRight.m_col - m_bottomLeft.m_col;
 }
 //------------------------------------------------------
 double Rectangle::getHeight() const{
 
-	return m_topRight.m_row - m_bottomLeft.m_row;
+     return m_topRight.m_row - m_bottomLeft.m_row;
 }
 //------------------------------------------------------
 void Rectangle::draw(Board& board) const {
@@ -90,15 +76,12 @@ void Rectangle::draw(Board& board) const {
 	board.drawLine(m_topRight, bottomRight);
 	board.drawLine(bottomRight, m_bottomLeft);
 	board.drawLine(m_bottomLeft, topLeft);
-
 }
 //------------------------------------------------------
 Rectangle Rectangle::getBoundingRectangle() const {
 	
-	//this will create a bounding rectangle and then return it
 	Rectangle boundingRectangle(m_bottomLeft, m_topRight);
 	return boundingRectangle;
-
 }
 //------------------------------------------------------
 double Rectangle::getArea() const{
@@ -116,41 +99,29 @@ Vertex Rectangle::getCenter() const {
 	double xCenter = (m_bottomLeft.m_col + m_topRight.m_col) / 2;
 	double yCenter = (m_bottomLeft.m_row + m_topRight.m_row) / 2;
 	
-	//this will return the center of the shape
 	Vertex center(xCenter, yCenter);
-	return center;
-		
+	return center;		
 }
 //------------------------------------------------------
-bool Rectangle::scale(double factor){
-	
-	Vertex center = getCenter();
-	Vertex bottomLeftTemp = centerToPoint(center, m_bottomLeft, factor, true);
-	Vertex topRightTemp = centerToPoint(center, m_topRight, factor, false);
-    
-	if (checkCond(bottomLeftTemp, topRightTemp))
-		return false;
+bool Rectangle::scale(double factor) {
 
-	m_bottomLeft = bottomLeftTemp;
-	m_topRight = topRightTemp;
+	Vertex center = getCenter();
+	Vertex bottomLeft(getScaleVertex(center, m_bottomLeft, factor));
+	Vertex topRight(getScaleVertex(center,m_topRight, factor));
+		
+	if (checkCond(bottomLeft, topRight))
+		return false;
+	
+	m_bottomLeft = bottomLeft;
+	m_topRight = topRight;
 	return true;
 }
 //------------------------------------------------------
-Vertex Rectangle::centerToPoint(Vertex center, Vertex point, double factor, bool botLeft) {
-
-	double xTemp, yTemp;
-	
-	if (botLeft) {
-		xTemp = center.m_col - abs((center.m_col - point.m_col)) * factor ;
-		yTemp = center.m_row - abs(center.m_row - point.m_row) * factor;
-	}
-	else {
-	   xTemp = abs((center.m_col - point.m_col)) * factor + center.m_col;
-	   yTemp = abs(center.m_row - point.m_row) * factor + center.m_row;
-	}
-	
-	Vertex temp(xTemp, yTemp);
-	return temp;
-
+Vertex Rectangle::getScaleVertex(Vertex center, Vertex vertex, double factor) {
+	//this function will return the the new scaled point
+	double col = center.m_col + factor * (vertex.m_col - center.m_col);
+	double row = center.m_row + factor * (vertex.m_row - center.m_row);
+	Vertex scaleVertex(col, row);
+	return scaleVertex;
 }
-
+  
